c539822fcd7afd7ee2e421aa49808803f2d4797f
Vyshnav A <vyshnavvishnu6@gmail.com>
2025-08-20 10:20:36 +0530

    initial

---
+++ 1st.js
@@ -0,0 +1,20 @@
+// es6 : spread operator
+let fruits = ["apple", "banana"]
+let veg = ["carrot", "chillies"]
+
+let products = [...fruits, ...veg]
+
+console.log(products)
+
+
+// es6 : object destructuring
+let car = {color: "white", make: 2025}
+
+let {color, make} = car
+
+console.log(color)
+
+let body = {name: "John", email: "mail@mail.com"}
+let {name} = body
+
+console.log(name)
